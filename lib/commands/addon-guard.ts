50865eaff5316ec39929daf9018ff7383d760726
step2yeung <syeung@linkedin.com>
2021-08-04 14:30:53 -0700

    Adding summary to output

--- lib/commands/addon-guard.ts
+++ lib/commands/addon-guard.ts
@@ -51,6 +51,19 @@ module.exports = {
 
     if (Object.keys(duplicateAddons).length > 0 || summary.errors.length > 0) {
       if (Object.keys(duplicateAddons).length > 0) {
+        this.ui.writeLine(chalk.red(chalk.underline('Summary')));
+        this.ui.writeLine(chalk.red(`Number of addons with multiple verions: ${ addonCount }\n`));
+        for (const name in duplicateAddons) {
+          const addonInstances: Dict<AddonVersionSummary> = duplicateAddons[name];
+          const versions = new Set();
+          for (const cacheKey of Object.keys(addonInstances)) {
+            const instance = addonInstances[cacheKey];
+            versions.add(instance.version);
+          }
+          this.ui.writeLine(chalk.red(`${name}: [${Array.from(versions.values()).join(' , ')}]`));
+        }
+
+        this.ui.writeLine(chalk.red(chalk.underline('\nDetails')));
         this.ui.writeLine(chalk.red(`ember-cli-addon-guard determined that your application is dependent on multiple versions of the following run-time ${ addonCount > 1 ? 'addons' : 'addon'}:\n`));
 
         for (const name in duplicateAddons) {
